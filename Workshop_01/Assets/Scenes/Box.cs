72e9eb337e0c71a7726f9dc96a2ec2bd4d76fc8e
NJ <nj.namju@gmail.com>
2019-07-06 21:56:25 +0900

    update

---
+++ Workshop_01/Assets/Scenes/Box.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour
+{
+    Transform t;
+    // Start is called before the first frame update
+    void Start()
+    {
+       t = gameObject.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        t.position = new Vector3(this.t.position.x + 0.05f, this.t.position.y, this.t.position.z);
+        Debug.Log("working!!");
+    }
+}
